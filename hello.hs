236c3f1bb58545c284e0e4cbfd6f60868a90f4b2
Lai-YT <381xvmvbib@gmail.com>
2022-01-28 00:12:16 +0800

    feat: interactive hello

--- hello.hs
+++ hello.hs
@@ -1,2 +1,12 @@
+import System.IO  -- hFlush
 
-main = putStrLn "Hello, world"
+
+main = do
+  -- putStr won't automatically break the line.
+  putStr "Please enter your name: "
+  -- putStr doesn't flush the buffer,
+  -- so you'll miss the output.
+  -- Manually flushes it.
+  hFlush stdout
+  name <- getLine
+  putStrLn ("hello, " ++ name)
